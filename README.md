f9d62032899ec770fdba05678c66a69046e7b05a
isamu-isozaki <isamu.website@gmail.com>
2024-04-20 18:41:19 -0400

    Finish model deployment

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ To start pretraining, assuming you have pytorch, do
 pip install accelerate timm diffusers pillow wandb transformers datasets
 accelerate launch pretraining.py
 ```
+
+### Deployment
+
+For deployment do
+
+```
+pip install python-dotenv flask langchain-openai
+python model_deployment.py
+```
--- model_deployment.py
+++ model_deployment.py
@@ -7,7 +7,8 @@ from PIL import Image
 from flask import Flask, jsonify, request
 import timm
 from torch import no_grad, inference_mode
-
+from PatientFriend.ChatPatient import ChatPatient
+# TODO: Grad cam is missing here and also do any possible optimizations
 
 app = Flask(__name__)
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -34,14 +35,14 @@ def get_prediction(image_bytes):
     return predicted_idx
 
 
-@app.route('/predict', methods=['POST', 'GET'])
+@app.route('/api/predict_tb', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
         file = request.files['file']
         img_bytes = file.read()
         class_id = get_prediction(image_bytes=img_bytes)
         return jsonify({'class_id': class_id})
-    # For referencing this flask server in the frontend, use a action="http://..../predict" in the form
+    # For referencing this flask server in the frontend, use a action="http://..../api/predict_tb" in the form
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -51,7 +52,42 @@ def predict():
       <input type=submit value=Upload>
     </form>
     '''
+chat_patient = ChatPatient()
 
+@app.route('/api/summarize', methods=['POST'])
+def summarize_text():
+    data = request.get_json()
+    report_text = data.get('text')
+
+    #Replace the double quotes
+    report_text = report_text.replace('"','')
+
+    if not report_text:
+        return jsonify({'error': 'No text provided'}), 400
+
+    try:
+        summary = chat_patient.get_friendly_text(report_text)
+        return jsonify({'summary': summary}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/api/translate', methods=['POST'])
+def translate_text():
+    data = request.get_json()
+    text = data.get('text')
+
+    #Replace the double quotes
+    text = text.replace('"','')
+
+    language = data.get('language', 'hindi')  # Default language is Hindi
+    if not text:
+        return jsonify({'error': 'No text provided'}), 400
+
+    try:
+        translated_text = chat_patient.translate_text(text, language=language)
+        return jsonify({'translated_text': translated_text}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
